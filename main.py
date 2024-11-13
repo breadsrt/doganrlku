3cd1f1fe74d5e26092dd9747e86672ff29790987
breadsroofses <yaroslavhorovyi@gmail.com>
2024-11-13 17:36:01 +0200

    save3

--- main.py
+++ main.py
@@ -17,6 +17,29 @@ class Hero:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d]:
             self.hitbox.x += self.speed
+        if keys[pygame.K_a]:
+            self.hitbox.x -= self.speed
+class Hero2:
+    def __init__(self, x,y, texture, width, height, speed):
+        self.image = pygame.image.load(texture)
+        self.image = pygame.transform.scale(self.image, [width, height])
+        self.hitbox = self.image.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+
+
+    def draw(self, window):
+        window.blit(self.image, [self.hitbox.x, self.hitbox.y])
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.hitbox.x += self.speed
+        if keys[pygame.K_LEFT]:
+            self.hitbox.x -= self.speed
+
+
 pygame.init()
 
 window = pygame.display.set_mode([700, 500])
@@ -25,6 +48,7 @@ fps = pygame.time.Clock()
 background = pygame.transform.scale(pygame.image.load("background.png"), [700, 500])
 
 player1 = Hero(250, 250, "sprite1.png", 50, 50, 10)
+player2 = Hero2(250, 250, "sprite2.png", 50, 50, 10)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -32,11 +56,13 @@ while True:
 
 
     player1.move()
+    player2.move()
 
 
     window.fill([0, 250, 0])
     window.blit(background, [0, 0])
     player1.draw(window)
+    player2.draw(window)
     pygame.display.flip()
 
     fps.tick(60)
